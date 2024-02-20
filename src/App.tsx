78baacb3e2de88aa8622df69d98568b6836e90cf
Hooman Khatibzadeh <hoomankhatibzadeh@gmal.com>
2024-02-20 11:58:12 +0330

    Refactor: Extract a query object

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,13 @@ import { Genres } from './hooks/useGenre';
 import PlatformSelector from './components/PlatformSelector';
 import { Platform } from './hooks/useGames';
 
+export interface GameQuery {
+	genre: Genres | null;
+	platform: Platform | null;
+}
+
 const App = () => {
-	const [selectedGenre, setSelectedGenre] = useState<Genres | null>(null);
-	const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
+	const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
 	return (
 		<Grid
@@ -28,17 +32,17 @@ const App = () => {
 			<Show above='lg'>
 				<GridItem area='aside' paddingX='20px'>
 					<GenreList
-						selectedGenre={selectedGenre}
-						onSelectedGenre={genre => setSelectedGenre(genre)}
+						selectedGenre={gameQuery.genre}
+						onSelectedGenre={genre => setGameQuery({ ...gameQuery, genre })}
 					/>
 				</GridItem>
 			</Show>
 			<GridItem area='main'>
 				<PlatformSelector
-					selectedPlatform={selectedPlatform}
-					onSelectedPlatform={platform => setSelectedPlatform(platform)}
+					selectedPlatform={gameQuery.platform}
+					onSelectedPlatform={platform => setGameQuery({ ...gameQuery, platform })}
 				/>
-				<GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
+				<GameGrid gameQuery={gameQuery} />
 			</GridItem>
 		</Grid>
 	);
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genres } from "./useGenre";
 
 export interface Platform {
 	id: number
@@ -16,16 +16,16 @@ export interface Games {
 }
 
 
-const useGames = (selectedGenre: Genres | null , selectedPlatform: Platform | null) =>
+const useGames = (gameQuery: GameQuery) =>
 
 useData<Games>(
 	'/games',
 
 	{params:
-		{genres: selectedGenre?.id, platforms: selectedPlatform?.id}
+		{genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id}
 	},
 
-	[selectedGenre?.id, selectedPlatform?.id]
+	[gameQuery]
 
 	)
 
